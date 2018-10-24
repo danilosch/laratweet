ec473780a36fc32e9be97f50daed4cff7f67748b
Danilo Schervinski <danilo_sch@live.com>
2018-10-24 15:30:16 -0300

    Create posts

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create(Request $request, Post $post)
+    {
+        $createdPost = $request->user()->posts()->create([
+            'body' => $request->body,
+        ]);
+
+        return response()->json($post->with('user')->find($createdPost->id));
+    }
+}
--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            body: ''
+        };
+        // bind
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.postData();
+        console.log(this.state.body);
+    }
+
+    handleChange(e) {
+        this.setState({
+            body: e.target.value
+        })
+    }
+
+    postData() {
+        axios.post('/posts', {
+            body: this.state.body
+        });
+    }
+
     render() {
         return (
             <div className="container">
@@ -10,9 +40,9 @@ class App extends Component {
                             <div className="card-header">Tweet something...</div>
 
                             <div className="card-body">
-                                <form>
+                                <form onSubmit={this.handleSubmit}>
                                     <div className="form-group">
-                                        <textarea className="form-control" rows="5" maxlenth="140" placeholder="Whats up?" required></textarea>
+                                        <textarea onChange={this.handleChange} className="form-control" rows="5" maxLength="140" placeholder="Whats up?" required></textarea>
                                     </div>
                                     <input type="submit" value="Post" className="form-control"></input>
                                 </form>
